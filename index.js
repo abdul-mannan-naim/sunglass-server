14cd92ec328b0da94fccb92fcea7fe26ed663866
abdul-mannan-naim <fahadmannan0001@gmail.com>
2023-02-21 22:44:33 +0600

    second

--- index.js
+++ index.js
@@ -1,38 +1,42 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
-require('dotenv').config(); 
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1zdgrcr.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
- 
+
 console.log(uri)
 
-async function run(){
+async function run() {
 
-    try{
-        const availableCollection = client.db("sunglass").collection("available");
-
-
-        
-        app.post('/addItem', async (req, res) => {
-            const query = req.body;
-            const result = await availableCollection.insertOne(query)
-            res.send(result)
-        })
+  try {
+    const availableCollection = client.db("sunglass").collection("available");
 
 
 
-    }
-    finally{
+    app.post('/addItem', async (req, res) => {
+      const query = req.body;
+      const result = await availableCollection.insertOne(query)
+      res.send(result)
+    })
+    app.get('/items', async (req, res) => {
+      const query = {}
+      const result = await availableCollection.find(query)
+      res.send(result)
+    })
 
-    }
+
+  }
+  finally {
+
+  }
 
 }
 
